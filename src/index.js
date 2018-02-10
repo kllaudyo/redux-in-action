e0a9a851af83560050ed1b5bbac7a038b57ad18f
Claudio Acioli <kllaudyo@gmail.com>
2018-02-10 10:58:04 -0200

    chore: Adiciona modulo quente

--- src/index.js
+++ src/index.js
@@ -2,12 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import './index.css';
 import tasks from './reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(tasks);
+const store = createStore(tasks, devToolsEnhancer());
+
+if(module.hot){
+    module.hot.accept('./App', ()=> {
+        const NextApp = require("./App").default;
+        ReactDOM.render(
+            <Provider store={store}>
+                <NextApp/>
+            </Provider>,
+            document.getElementById('root')
+        );
+    });
+
+    module.hot.accept('./reducers', () => {
+        const nextRootReducer = require('./reducers').default;
+        store.replaceReducer(nextRootReducer);
+    });
+}
 
 ReactDOM.render(
     <Provider store={store}>
