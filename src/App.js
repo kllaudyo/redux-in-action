51c01e3f18139181100a8cca12c8715fd223447a
Claudio Acioli <kllaudyo@gmail.com>
2018-02-09 23:18:04 -0200

    feat: Adiciona redutor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from 'react';
 import TaskPage from './components/TaskPage';
 
-const mockTasks = [
-    {
-        id: 1,
-        title: 'Learn Redux',
-        description: 'The store, actions, and reducers, oh my!',
-        status: 'In Progress',
-    },
-    {
-        id: 2,
-        title: 'Peace on Earth',
-        description: 'No big deal.',
-        status: 'In Progress',
-    },
-];
-
 class App extends Component {
   render() {
     return (
